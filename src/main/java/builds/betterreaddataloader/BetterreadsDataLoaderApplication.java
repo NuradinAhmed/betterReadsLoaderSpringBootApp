c346e8095a653eeb3e4bffd14b8b7e926cc711e8
NuradinAhmed <Nuradinahmed2012@gmail.com>
2021-10-17 19:19:04 +0300

    first commit

--- src/main/java/builds/betterreaddataloader/BetterreadsDataLoaderApplication.java
+++ src/main/java/builds/betterreaddataloader/BetterreadsDataLoaderApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BetterreadsDataLoaderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BetterreaddataloaderApplication.class, args);
+		SpringApplication.run(BetterreadsDataLoaderApplication.class, args);
 	}
 
 }
