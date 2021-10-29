5745904aef80d8f91113a4c38afb67a8be096210
Nuradin Ahmed <nuradinahmed2012@gmail.com>
2021-10-29 19:10:54 +0300

    created bookRepo and its athors

--- src/main/java/builds/betterreaddataloader/BetterreadsDataLoaderApplication.java
+++ src/main/java/builds/betterreaddataloader/BetterreadsDataLoaderApplication.java
@@ -1,13 +1,20 @@
 package builds.betterreaddataloader;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.annotation.PostConstruct;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +27,8 @@ import org.springframework.context.annotation.Bean;
 
 import builds.betterreaddataloader.author.Author;
 import builds.betterreaddataloader.author.AuthorRepository;
+import builds.betterreaddataloader.book.Book;
+import builds.betterreaddataloader.book.BookRepository;
 
 
 
@@ -28,7 +37,10 @@ import builds.betterreaddataloader.author.AuthorRepository;
 public class BetterreadsDataLoaderApplication {
 
 
-	@Autowired AuthorRepository authorRepository;
+	@Autowired AuthorRepository authorRepository; //local variable and then autowired for author repo
+
+
+	@Autowired BookRepository bookRepository;   //local varialbe and then autowired for book repo
 
 
 	//creating @value annotation that will allow me to grab locatoin path data and pass it to these property variables in memeory 
@@ -53,7 +65,7 @@ public class BetterreadsDataLoaderApplication {
 		try (Stream<String> lines = Files.lines(path)) { // I am going to go each stream of lines in the path given from
 															// authors file
 
-			lines.limit(10).forEach(line -> { // i am going to get each line - am going to test and good way is to put
+			lines.limit(10000).forEach(line -> { // i am going to get each line - am going to test and good way is to put
 												// limit in a stream like 10 records
 				// 1-Read and parse the line
 				String jsonString = line.substring(line.indexOf("{")); // Returns the index within this string of the
@@ -94,9 +106,133 @@ public class BetterreadsDataLoaderApplication {
 
 
 	private void initWorks() {
-		Path path = Paths.get(worksDumpLocation);
+		Path paths = Paths.get(worksDumpLocation); // I have the path of the worksDumpLoaction
+
+
+		//This will be used for formatting the dateformatter parser Creates a formatter using the specified pattern.
+		//This method will create a formatter based on a simple pattern of letters and symbols as described in the class documentation. 
+			//For example, d MMM uuuu will format 2011-12-03 as '3 Dec 2011'.
+		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS"); //then assign to dateFormat and pass it to anywhere am parsing the date
+
+		// I am going to get each line of the path given above as stream of lines
+		try (Stream<String> lines = Files.lines(paths)) { // read all lines from a file as stream.
+
+			// I am going to get each line - iterate over
+			lines.limit(200).forEach(line -> { // Read and parse the lines
+
+				// Read and parse the line
+				String jsonString = line.substring(line.indexOf("{")); // Returns the index within this string of the
+																		// first occurrence of
+																		// the specified { curly braces to get the json
+																		// part of the data
+
+				// We then convert the string Above to JSON OJbect using JSON API.
+				try {
+
+					JSONObject jsonObject = new JSONObject(jsonString);
+
+					// Once i have as JSON Object of the intWorks(book info) I need to save it
+
+					// Construct book object //am going to parse each line above file and then put
+					// into the book object
+					Book book = new Book();
+					book.setId(jsonObject.getString("key").replace("/works/", ""));
+
+					book.setName(jsonObject.optString("title"));
+
+					// first i need to get the description which is json object and i need value
+					// from there on. ex. "description": {"type": "/type/text", "value": "Adam and
+					// his"}
+					JSONObject descriptionObj = jsonObject.optJSONObject("descriptioin"); // we are getting the JSON
+																							// Ojbect of description
+					if (descriptionObj != null) { // and then here we checkig if it exist and if it does
+						book.setDescription(descriptionObj.optString("value")); // then we are getting the string value
+					}
+
+					JSONObject publishedObj = jsonObject.optJSONObject("created"); // and here we are getting the actaul
+																					// JSON Object created
+					if (publishedObj != null) { // checking if it exist and if it does
+						String dateStr = publishedObj.optString("value"); // then we are getting the value from the json
+																			// as string
+						book.setPublishedDate(LocalDate.parse(dateStr, dateFormat)); // and then we are parsing it here - converting
+																			// it to localdate and setting it to
+																			// publishedate.
+					}
+
+					JSONArray coversJSONArr = jsonObject.optJSONArray("covers");
+					if (coversJSONArr != null) {
+						List<String> coverIds = new ArrayList<>(); // list of an arraylist as string . ex. coverIds:
+																	// "covers": [4567, 67888, 34556] - as you can see
+																	// its a list of string.An arrayList
+						for (int i = 0; i < coversJSONArr.length(); i++) {
+
+							// each string i get from the loop above am adding to the coverIds property
+							// which in itself is arrayList.
+							coverIds.add(coversJSONArr.getString(i)); // get json array using get string method. tekes
+																		// an index give it back a string .
+						}
+
+						book.setCoverIds(coverIds);
+					}
+
+					JSONArray authorJsonArray = jsonObject.optJSONArray("authors");
+					if (authorJsonArray != null) {
+						List<String> authorIds = new ArrayList<>(); // creating here authorIds as an arrayList
+						for (int i = 0; i < authorJsonArray.length(); i++) {
+
+							// for each iteration i need to get the author object which is the i index of
+							// the authorJsonarray
+							String authorId = authorJsonArray.getJSONObject(i). // and finally the result put into field
+																				// authroId of string.
+							getJSONObject("author"). // then am gong to get the JSON object within it named author
+							getString("Key"). // and and then from there get the string key -
+							replace("/authors/", ""); // to get the key we need to replace "/authors/" infront of it
+														// with empty string/character and only get the key after it .
+
+							authorIds.add(authorId); // so finally am adding the authorid string into the authorIds
+														// array - stores as memorty and its defined above as an
+														// arrayList so i can acess it
+						}
+
+						book.setAuthorIds(authorIds);
+
+						// ------------------------------How do we get the author names -
+						// we need to make a call to our author repository in cassandra and say hey here
+						// is the authorId give me the corresponding authorname so that i can save into
+						// the book repository
+						List<String> authorNames = authorIds.stream().map(id -> authorRepository.findById(id))
+								.map(optionalAuthor -> {
+									if (!optionalAuthor.isPresent())
+										return "Unknown Author"; // if it cannot find the authorName then return unknown
+									return optionalAuthor.get().getName(); // otherwise get me the author name.
+								}).collect(Collectors.toList());
+
+						book.setAuthorNames(authorNames);
+
+						// Persisit using Repository
+						System.out.println("Saving book " + book.getName() + "...");
+						bookRepository.save(book);
+
+
+					}
+
+				} catch (Exception e) {
+					// handle the exception by printin it
+					e.printStackTrace();
+				}
+
+			}
+
+			);
+
+		} catch (IOException e) {
+			// hndle the exception - am just gonna print the exception
+			e.printStackTrace();
+		}
 
 	}
+
+
 
 
 
@@ -104,7 +240,7 @@ public class BetterreadsDataLoaderApplication {
 	@PostConstruct
 	public void start() { 
 		//initAuthors method will be called in here
-		initAuthors();
+		//initAuthors(); //comment not to run again when pushed all data.
 		initWorks();
 		
 		/* testing below 
